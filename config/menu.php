7acfa1a469e7e34781a5e9099d5dfdcf65af01d2
ploytawan <poliplooy@gmail.com>
2024-06-28 17:11:31 +0700

    commit

---
+++ config/menu.php
@@ -0,0 +1,28 @@
+<?php
+return [
+    [
+        'name' => 'Dashboard',
+        'icon' => 'bx bx-home',
+        'url' => ['/','InsertDevice'],
+    ],
+    /* [
+        'name' => 'Monitor',
+        'icon' => 'bx bx-objects-vertical-bottom',
+        'url' => '/#',
+    ],
+    [
+        'name' => 'Insights',
+        'icon' => 'bx bx-color',
+        'url' => '/#',
+    ],
+    [
+        'name' => 'Docs',
+        'icon' => 'bx bx-money-withdraw',
+        'url' => '/#',
+    ],
+    [
+        'name' => 'Config',
+        'icon' => 'bx bxl-gmail',
+        'url' => '/#',
+    ], */
+];
